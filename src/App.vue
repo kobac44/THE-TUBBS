b2dc457940e45be373ed040173506bbb90ba8172
kobie watkins <kobie>
2021-12-24 13:24:01 -0500

    new routes, add font awesome

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/drumbook">DRUMBOOK</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/contact">Contact</router-link> 
+    </div>
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
 <style>
---
+++ src/views/DrumBook.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="drumbook">
+    <h1>This is an drumbook page</h1>
+  </div>
+</template>

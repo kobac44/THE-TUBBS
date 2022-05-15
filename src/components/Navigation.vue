e5a4f1c39315c1f3e3a23169d452f652a5b527f0
kobie watkins <kobie>
2022-05-15 00:26:48 -0400

    added functions for the hambuger to toggle, added a scroll, addedsmooth transition for the mobile nav, added listner events for the home screen window

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-    <header :class="{ 'scrolled-nav': scrollPosition }">
+    <header :class="{ 'scrolled-nav': scrolledNav }">
         <nav>
             <div class="branding">
                 <img scr="" alt="">
@@ -36,20 +36,55 @@
 </template>
 
 <script>
+
 export default {
     name: "navigation",
     data() {
       return {
-          scrollPosition: null,
-          mobile: true,
-          mobileNav: true,
+          scrolledNav: null,
+          mobile: null,
+          mobileNav: null,
           windowWidth: null,
       };  
+    },
+    created() {
+       window.addEventListener("resize", this.checkScreen);
+       this.checkScreen();
+    },
+    mounted() {
+        window.addEventListener('scroll', this.updateScroll);
+
+    },
+    methods: {
+        toggleMobileNav() {
+            this.mobileNav = !this.mobileNav;
+        },
+
+        updateScroll() {
+            const scrollPosition = window.scrollY;
+            if (scrollPosition > 50) {
+                this.scrolledNav = true;
+                return;
+            }
+            this.scrolledNav = false;
+        },
+
+        checkScreen() {
+            this.windowWidth = window.innerWidth;
+            if (this.windowWidth <= 750){
+                this.mobile = true;
+                return;
+            }
+            this.mobile = false;
+            this.mobileNav = false;
+            return;
+        },
     },
 };
 </script>
 
 <style scoped>
+
 nav {
     position: relative;
     padding: 12px 0;
@@ -103,13 +138,11 @@ li {
     height: 100%;
 }
 
-.icon {
+.icon 
+.icon-active{
     cursor: pointer;
-    font-size: 24px;
+    font-size: 28px;
     transition: .8s ease all;
-}
-
-.icon-active {
     transform: rotate(180deg);
 }
 
@@ -120,7 +153,7 @@ li {
     width: 100%;
     max-width: 250px;
     height: 100%;
-    background-color: darkolivegreen;
+    background-color: rgb(204, 224, 168);
     top: 0;
     left: 0;
 }
@@ -129,8 +162,31 @@ li {
     margin-left: 0;
 }
 
-.link {
-    color: #000
+.mobile-nav-enter-active,
+.mobile-nav-leave-active {
+    transition: 1s ease all;
 }
+
+.mobile-nav-enter-from,
+.mobile-nav-leave-to {
+    transform: translateX(-250px);
+}
+
+.mobile-nav-enter-to {
+    transform: translateX(0);
+}
+
+.scrolledNav {
+    background-color: #000;
+    box-shadow: 0 4px 6px -1  rgba(0, 0, 0 , 0.1), 0 2px 4px -1px  rgba(0, 0, 0, 0.06);
+}
+
+nav {
+    padding: 8px 0;
+}
+
+
+
 </style>
+
 

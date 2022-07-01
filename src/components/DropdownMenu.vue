a9ee5f58300963bd360b4495e0a48e4eac593190
kobie watkins <kobie.watkins@gmail.com>
2022-07-01 14:04:37 -0400

    added some styling to the nav.sub-menu

--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -6,11 +6,13 @@
         <svg viewBox="0 0 1030 638" width="10">
             <path d="M1017 68L541 626q-11 12-26 12t-26-12L13 68Q-3 49 6 24.5T39 0h952q24 0 33 24.5t-7 43.5z" fill="#FFF"></path>
         </svg>
-        <div class="sub-menu">
+        <transition>
+          <div class="sub-menu">
             <div v-for="(item, i) in items" :key="i" class="menu-item">
                 <a :href="item.link">{{ item.title}}</a>
             </div>
-        </div>
+          </div>
+        </transition>
     </div>
 </template>
 
@@ -24,11 +26,15 @@ export default {
 <style scope>
 nav .menu-item svg {
     width: 10px;
-    margin-left: 10px;  
+    margin-left: 10px; 
 }
 
-div.menu-item {
-    
+nav .menu-item .sub-menu {
+    position: absolute;
+    /* top: calc(100% + 18px); */
+    transform: translate(-50);
+    width: max-content;
+    border-radius: 0px 0px 16px 16px;
 }
 
 
